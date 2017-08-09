859e6413ca43bf611eece51817d6f9dbe6ca0aff
Sharonsyra <sharonwaithii@gmail.com>
2017-08-09 21:03:43 +0300

    Create class Bucketlist

--- classes/bucketlist.py
+++ classes/bucketlist.py
@@ -1,11 +1,12 @@
 class Bucketlist(object):
     """ Class Bucketlist belongs to a user """
 
-    def __init__(self, name, done, date_created, date_modified, items=[]):
+    def __init__(self, name, date_created, date_modified, items=[]):
         self.id = id(self)
         self.name = name
         self.date_created = date_created
         self.date_modified = date_modified
+        self.items = items
 
     def __repr__(self):
         return "<BucketList {}>".format(self.name)
